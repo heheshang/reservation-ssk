4d57b1870ced7bae32861fc2fe1c576e44beed57
heheshang <2448318433@qq.com>
2022-11-22 15:41:21 +0800

    add proto file

---
+++ abi/build.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+
+fn main() {
+    tonic_build::configure()
+        .out_dir("src/pb")
+        .compile(&["protos/reservation.proto"], &["protos"])
+        .unwrap();
+    // fs::remove_file("path/to/file").unwrap();
+
+    Command::new("cargo").args(&["fmt"]).output().unwrap();
+
+    println!("cargo:rerun-if-changed=protos/reservation.proto");
+
+    // tonic_build::configure()
+    //     .out_dir("src/pb")
+    //     .compile(&["protos/reservation.proto"], &["protos"])
+    //     .unwrap();
+
+    // Command::new("cargo").args(&["fmt"]).output().unwrap();
+
+    // println!("cargo:rerun-if-changed=protos/reservation.proto");
+}
